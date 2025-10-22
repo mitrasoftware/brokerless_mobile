79a2593febbd992ee913cf6d9198ba369bc96e2f
irshad Chanakya <35327176+mitrasoftware@users.noreply.github.com>
2025-10-22 20:18:20 +0530

    routes

--- projects_build/RC_MOBILES/routes/routes.go
+++ projects_build/RC_MOBILES/routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	auth "github.com/mitrasoftware/pureone_backend_go/auth"
 	ctr "github.com/mitrasoftware/pureone_backend_go/controllers/add"
+	fetch "github.com/mitrasoftware/pureone_backend_go/controllers/fetch"
 	"github.com/mitrasoftware/pureone_backend_go/middleware"
 	// dtl "github.com/mitrasoftware/brokerless/controllers/remove"
 	// upd "github.com/mitrasoftware/brokerless/controllers/update"
@@ -19,7 +20,11 @@ func SetupRoutes() *gin.Engine {
 	auth := r.Group("/api")
 	auth.Use(middleware.AuthMiddleware())
 
-	auth.POST("/list_services", ctr.AddServices)
+	auth.GET("/fetch_category_icons", fetch.CategoryIcons)
+	auth.POST("/add_services", ctr.AddServices)
+
+	auth.POST("/add_products", ctr.AddProducts)
+	auth.GET("/get_products", fetch.GetProducts)
 
 	return r
 	// }
